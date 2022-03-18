7617ce54d17ee28bf016870212b6e09131d3c7b8
zoLikeCode <70718862+zoLikeCode@users.noreply.github.com>
2022-03-18 15:55:33 +0300

    Update settings.py

--- machineLearning/settings.py
+++ machineLearning/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qz$3%=2^3m)zzfw8+iv)t(43x8#5%g#jjptk6@78@v3%kk1g!a'
+SECRET_KEY = ${{secrets.SECRET_KEY_APP}}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
